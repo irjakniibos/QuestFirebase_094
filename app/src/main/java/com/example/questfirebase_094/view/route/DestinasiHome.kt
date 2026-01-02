9b116f16f3f753df8136c971db4dbc2b10270f38
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-02 19:14:12 +0700

    Membuat objek DestinasiHome

---
+++ app/src/main/java/com/example/questfirebase_094/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_094.view.route
+
+import com.example.questfirebase_094.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
