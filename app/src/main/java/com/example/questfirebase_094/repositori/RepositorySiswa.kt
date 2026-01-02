1b74d3ed44a21f1a504d03102bd0d73f6125eba4
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-02 18:45:56 +0700

    Memperbaiki import Siswa dan merapikan kode

--- app/src/main/java/com/example/questfirebase_094/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase_094/repositori/RepositorySiswa.kt
@@ -1,6 +1,6 @@
 package com.example.questfirebase_094.repositori
 
-import com.example.firebase.modeldata.Siswa
+import com.example.questfirebase_094.modeldata.Siswa
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
@@ -13,7 +13,7 @@ class FirebaseRepositorySiswa : RepositorySiswa {
     private val db = FirebaseFirestore.getInstance()
     private val collection = db.collection("siswa")
 
-    override suspend fun getDataSiswa(): List<Siswa> {
+    override suspend fun getDataSiswa(): List<Siswa>{
         return try {
             collection.get().await().documents.map { doc ->
                 Siswa(
@@ -29,17 +29,13 @@ class FirebaseRepositorySiswa : RepositorySiswa {
     }
 
     override suspend fun postDataSiswa(siswa: Siswa) {
-        val docRef =
-            if (siswa.id == 0L) collection.document()
-            else collection.document(siswa.id.toString())
-
+        val docRef = if (siswa.id == 0L) collection.document() else collection.document (siswa.id.toString())
         val data = hashMapOf(
             "id" to (siswa.id.takeIf { it != 0L } ?: docRef.id.hashCode()),
             "nama" to siswa.nama,
             "alamat" to siswa.alamat,
             "telpon" to siswa.telpon
         )
-
         docRef.set(data).await()
     }
 }
