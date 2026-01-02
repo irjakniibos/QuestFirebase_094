08a17acf47db1da4d385d127c4644c2bad27c788
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-02 19:21:09 +0700

    Membuat DataSiswaApp sebagai composable utama aplikasi

--- app/src/main/java/com/example/questfirebase_094/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase_094/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questfirebase_094.ui.theme.QuestFirebase_094Theme
+import com.example.questquestfirebase_094_094.view.controlNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestFirebase_094Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
