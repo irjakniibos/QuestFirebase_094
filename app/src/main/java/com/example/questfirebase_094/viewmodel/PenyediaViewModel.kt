6fe8ee99dba4abf9b9f8a16e1c1eb92317891464
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-02 19:08:52 +0700

    Membuat PenyediaViewModel untuk ViewModelFactory

---
+++ app/src/main/java/com/example/questfirebase_094/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.questfirebase_094.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questfirebase_094.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+                AplikasiDataSiswa
+        )
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+    }
+}
