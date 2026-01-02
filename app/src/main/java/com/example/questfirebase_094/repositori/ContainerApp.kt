4c2d819c8597962f0c8a8bc2707ea36d48e8f272
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-02 18:39:56 +0700

    Membuat implementasi default untuk `ContainerApp` yaitu `DefaultContainerApp` dan menginisialisasi `FirebaseRepositorySiswa`.

--- app/src/main/java/com/example/questfirebase_094/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questfirebase_094/repositori/ContainerApp.kt
@@ -5,3 +5,9 @@ import android.app.Application
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
