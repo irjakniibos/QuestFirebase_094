8ee4e92da89819d6157eb70a1162ed946db4c6ec
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-08 11:49:29 +0700

    Memperbarui PenyediaViewModel untuk handle state ViewModel Detail dan Edit

--- app/src/main/java/com/example/questfirebase_094/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_094/viewmodel/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.questquestfirebase_094_094.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questfirebase_094.repositori.AplikasiDataSiswa
+import com.example.questfirebase_094.viewmodel.DetailViewModel
+import com.example.questfirebase_094.viewmodel.EditViewModel
 import com.example.questfirebase_094.viewmodel.HomeViewModel
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
@@ -14,13 +17,17 @@ fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer {
-            HomeViewModel(
+            DetailViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
         initializer {
-            EntryViewModel(
+            EditViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
