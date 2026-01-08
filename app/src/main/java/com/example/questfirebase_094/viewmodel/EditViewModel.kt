652d9181911a92667fed5a8c14a146ad6d45ada4
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-08 11:25:39 +0700

    Menambahkan fungsi `editSatuSiswa` pada EditViewModel

--- app/src/main/java/com/example/questfirebase_094/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_094/viewmodel/EditViewModel.kt
@@ -25,6 +25,14 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
-
-
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa())
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
 }
