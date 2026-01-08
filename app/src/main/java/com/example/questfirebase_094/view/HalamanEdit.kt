83670eb71949bec1bd4b7a8ce873c056a9d5f637
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-08 11:41:08 +0700

    Membuat file HalamanEdit.kt

---
+++ app/src/main/java/com/example/questfirebase_094/view/HalamanEdit.kt
@@ -0,0 +1,10 @@
+package com.example.questfirebase_094.view
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
