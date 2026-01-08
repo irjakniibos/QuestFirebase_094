a0275e5623803d7fba0c95cae4d791805e096198
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-08 11:24:33 +0700

    Memuat data siswa di EditViewModel

--- app/src/main/java/com/example/questfirebase_094/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_094/viewmodel/EditViewModel.kt
@@ -4,5 +4,16 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
 
 }
