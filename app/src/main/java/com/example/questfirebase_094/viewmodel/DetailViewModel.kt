72b2c837079cc8bed19af335b1e5fc4337b95782
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-01-08 11:19:03 +0700

    Membuat DetailViewModel dan mengambil idSiswa dari SavedStateHandle

--- app/src/main/java/com/example/questfirebase_094/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_094/viewmodel/DetailViewModel.kt
@@ -10,3 +10,18 @@ sealed interface StatusUIDetail {
     object Loading : StatusUIDetail
 }
 
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa) : ViewModel() {
+
+    private val idSiswa: Long =
+        checkNotNull(savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong())
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+
+}
